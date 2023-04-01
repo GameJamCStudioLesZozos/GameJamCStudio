88a972dc766bac027f8ab0c7654e5b2919555b33
Srykah <srykah@gmail.com>
2023-04-01 23:42:49 +0200

    Fix crash foe spawning

--- Scripts/FoeSpawner.cs
+++ Scripts/FoeSpawner.cs
@@ -5,12 +5,13 @@ using System.IO;
 
 public partial class FoeSpawner : Node2D
 {
-	[Export] public float baseTimer = 10.0f;
+	[Export] public double baseTimer = 10.0;
 	[Export] public float spawnOffset = 0.0f;
 	[Export] public Node2D player;
-	[Export] public PackedScene Foe;
+	[Export] public PackedScene IceFoe;
+	[Export] public PackedScene FireFoe;
 
-	private float timer;
+	private double timer;
 	private RandomNumberGenerator rng = new();
 
 	// Called when the node enters the scene tree for the first time.
@@ -22,8 +23,8 @@ public partial class FoeSpawner : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		timer -= (float)delta;
-		if (timer < 0.0f)
+		timer -= delta;
+		if (timer < 0.0)
 		{
 			Spawn();
 			timer = baseTimer;
@@ -32,20 +33,18 @@ public partial class FoeSpawner : Node2D
 
 	public bool Spawn()
 	{
-		PackedScene prefab = Foe;
+		PackedScene prefab = null;
 		if (rng.Randf() > 0.5f)
 		{
-			prefab = Foe;
+			prefab = IceFoe;
 		}
 		else
 		{
-			//prefab = ResourceLoader.Load<PackedScene>(Foes[1]);
+			prefab = FireFoe;
 		}
 		Node2D monster = (Node2D)prefab.Instantiate();
-		int rand = (int)GD.Randi() % 2;
-		Node2D monsterToSpawn = (Node2D)monster.GetChild(rand);
-		monsterToSpawn.Reparent(this);
-		monster.QueueFree();
+		GetParent().AddChild(monster);
+
 		var viewportSize = GetViewport().GetVisibleRect().Size;
 		var newPos = player.Position;
 		if (rng.Randf() > 0.5f)
@@ -60,8 +59,7 @@ public partial class FoeSpawner : Node2D
 			newPos.X += (rng.Randf() > 0.5f ? +1.0f : -1.0f) * (0.5f * viewportSize.X + spawnOffset);
 			newPos.Y += (rng.Randf() - 0.5f) * viewportSize.Y;
 		}
-		monsterToSpawn.Transform = new Transform2D(0, newPos);
-		//this.AddChild(monsterToSpawn);
+		monster.Transform = new Transform2D(0, newPos);
 		
 		return true;
 	}
