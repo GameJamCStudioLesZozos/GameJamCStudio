5e8d0b42a669ea985717e08e22a255856ed8cb7a
StarSlash <StarSlash@CLEM.local>
2023-04-01 20:13:06 +0200

    Foe are destroyed according to their type

--- Scripts/BallController.cs
+++ Scripts/BallController.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using Godot.Collections;
+using Array = Godot.Collections.Array;
 
 public partial class BallController : RigidBody2D
 {
@@ -14,9 +16,20 @@ public partial class BallController : RigidBody2D
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
+		var oldPos = this.Position;
 		angle += rotationSpeed * delta;
-		this.Position = new Vector2((float)Math.Sin(angle), (float)Math.Cos(angle)) * radius;
+		var newPos = new Vector2((float)Math.Sin(angle), (float)Math.Cos(angle)) * radius;
+		
+		var collision = MoveAndCollide(newPos - oldPos, true);
+		Position = newPos;
+		if (collision != null)
+		{
+			if (collision.GetCollider().HasMethod("Hit"))
+			{
+				collision.GetCollider().Call("Hit",Variant.From(type));
+			}
+		}
 	}
 }
--- Scripts/Foe.cs
+++ Scripts/Foe.cs
@@ -4,6 +4,8 @@ using System;
 public partial class Foe : AnimatableBody2D
 {
 	[Export] public float Speed = 500;
+	[Export] public float StonksCooldown = 2.0f;
+	public float ActualCooldown;
 	
 	public Node2D player;
 	private Vector2 direction;
@@ -14,16 +16,30 @@ public partial class Foe : AnimatableBody2D
 	{
 		player = GetNode<Node2D>("/root/Node2D/Player");
 		direction = (player.Position - this.Position).Normalized();
+		ActualCooldown = StonksCooldown;
 	}
 
-	void Hit(Vector2 position, float direction)
+	void Hit(Type typeofBall)
 	{
-		
+		if (typeofBall == this.type)
+		{
+			if (ActualCooldown < 0.0f)
+			{
+				GD.Print("Getting Stronger");
+				ActualCooldown = StonksCooldown;
+				//TODO
+			}
+		}
+		else
+		{
+			QueueFree();
+		}
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		direction = (player.Position - this.Position).Normalized();
-		MoveAndCollide(direction*Speed*(float)delta);
+		KinematicCollision2D collision = MoveAndCollide(direction*Speed*(float)delta);
+		ActualCooldown -= (float)delta;
 	}
 }
