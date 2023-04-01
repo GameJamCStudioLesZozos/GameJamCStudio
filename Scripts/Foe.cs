48ded58f8d5a32b104c97d1b803df423b5ed2333
Srykah <srykah@gmail.com>
2023-04-01 23:44:24 +0200

    Player takes damage

--- Scripts/Foe.cs
+++ Scripts/Foe.cs
@@ -7,6 +7,9 @@ public partial class Foe : AnimatableBody2D
 	[Export] public float StonksCooldown = 2.0f;
 	public float ActualCooldown;
 	
+	[Export] public int Damage = 10;
+	[Export] public int DamageBonus = 5;
+
 	public Node2D player;
 	private Vector2 direction;
 	
@@ -27,7 +30,7 @@ public partial class Foe : AnimatableBody2D
 			{
 				GD.Print("Stonks");
 				ActualCooldown = StonksCooldown;
-				//TODO
+				Damage += DamageBonus;
 			}
 		}
 		else
@@ -41,5 +44,12 @@ public partial class Foe : AnimatableBody2D
 		direction = (player.Position - this.Position).Normalized();
 		KinematicCollision2D collision = MoveAndCollide(direction*Speed*(float)delta);
 		ActualCooldown -= (float)delta;
+		if (collision != null)
+		{
+			if (collision.GetCollider().HasMethod("TakeDamage"))
+			{
+				collision.GetCollider().Call("TakeDamage", Damage);
+			}
+		}
 	}
 }
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -9,6 +9,11 @@ public partial class PlayerController : CharacterBody2D
     public int MaxHealth = 100;
 
     [Export]
+    private double InvulnerabilityCooldown = 3.0;
+
+    private double CurrentInvulnerabilityDuration = 0.0;
+
+    [Export]
     public int Speed { get; set; } = 400;
 
     [Signal]
@@ -20,6 +25,17 @@ public partial class PlayerController : CharacterBody2D
     public override void _Ready()
     {
         Health = MaxHealth;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        CurrentInvulnerabilityDuration -= delta;
+        if (CurrentInvulnerabilityDuration <= 0.0)
+        {
+            CurrentInvulnerabilityDuration = 0.0;
+        }
     }
 
     public void GetInput()
@@ -36,15 +52,22 @@ public partial class PlayerController : CharacterBody2D
 
     public void TakeDamage(int amount)
     {
+        if (CurrentInvulnerabilityDuration > 0.0)
+        {
+            return; // still invincible
+        }
+
+        CurrentInvulnerabilityDuration = InvulnerabilityCooldown;
+        
         Health -= amount;
-        if (Health <= 0)
+        if (Health < 0)
         {
             Health = 0;
-            EmitSignal(SignalName.Died);
         }
-        else
+        EmitSignal(SignalName.HealthChanged, Health);
+        if (Health == 0)
         {
-            EmitSignal(SignalName.HealthChanged, Health);
+            EmitSignal(SignalName.Died);
         }
     }
 }
