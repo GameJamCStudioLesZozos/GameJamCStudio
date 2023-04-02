11a76b731c135340f7f3849122c58c37c8a30b9a
Arthur Cousseau <arthur.cousseau@me.com>
2023-04-02 18:50:42 +0200

    Add instructions to intro

--- Scripts/IntroScript.cs
+++ Scripts/IntroScript.cs
@@ -2,9 +2,21 @@ using Godot;
 
 public partial class IntroScript : Node2D
 {
+    [Export] public Control howToPlayParent;
+
     public void OnPlayButtonUp()
     {
         GetTree().ChangeSceneToFile("res://Scenes/main.tscn");
+    }
+
+    public void OnShowHowToPlayButtonUp()
+    {
+        howToPlayParent.Visible = true;
+    }
+
+    public void OnHideHowToPlayButtonUp()
+    {
+        howToPlayParent.Visible = false;
     }
 
     public void OnQuitButtonUp()
