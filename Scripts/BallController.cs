b76363b7c32b2809035e62b741226fa5515a1f50
Srykah <srykah@gmail.com>
2023-04-02 13:35:24 +0200

    Hitboxes and Hurtboxes 2

--- Scripts/BallController.cs
+++ Scripts/BallController.cs
@@ -1,13 +1,14 @@
 using Godot;
 using System;
 
-public partial class BallController : RigidBody2D
+public partial class BallController : Node2D
 {
 	[Export] public double angle = 0;
 	[Export] public double rotationSpeed = 1.5;
 	[Export] public float radius = 100;
 	[Export] public Type type;
 	[Export] public int damage = 10;
+	[Export] private Area2D hitbox;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -15,25 +16,35 @@ public partial class BallController : RigidBody2D
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double delta)
+	public override void _Process(double delta)
 	{
 		var oldPos = this.Position;
 		angle += rotationSpeed * delta;
-		var newPos = new Vector2((float)Math.Sin(angle), (float)Math.Cos(angle)) * radius;
-		
-		var collision = MoveAndCollide(newPos - oldPos, true);
-		Position = newPos;
-		if (collision != null)
-		{
-			if (collision.GetCollider().HasMethod("Hit"))
-			{
-				collision.GetCollider().Call("Hit", Variant.From(type), damage);
-			}
-		}
+		Position = new Vector2((float)Math.Sin(angle), (float)Math.Cos(angle)) * radius;
 	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+		CheckHitBoxCollisions();
+    }
 
     public void OnPlayerDied()
     {
         QueueFree();
     }
+
+	public void CheckHitBoxCollisions()
+	{
+		var hurtboxes = hitbox.GetOverlappingAreas();
+		foreach (var hurtbox in hurtboxes)
+		{
+			var parent = hurtbox.GetParent();
+			if (parent != null && parent.HasMethod("Hit"))
+			{
+				parent.Call("Hit", Variant.From(type), damage);
+			}
+		}
+	}
 }
