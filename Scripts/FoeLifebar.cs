9fa90012da581cfd51035be464f8a8d6c9027fb8
StarSlash <StarSlash@CLEM.local>
2023-04-02 16:17:27 +0200

    Added damage taken on hit on monsters

--- Scripts/FoeLifebar.cs
+++ Scripts/FoeLifebar.cs
@@ -7,6 +7,8 @@ public partial class FoeLifebar : Control
 
 	[Export] private TextureProgressBar lifebarProgress;
 
+	[Export] private Label DamageTakenText;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -18,11 +20,20 @@ public partial class FoeLifebar : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (DamageTakenText.SelfModulate.A > 0)
+		{
+			DamageTakenText.SelfModulate = new Color(DamageTakenText.SelfModulate, DamageTakenText.SelfModulate.A-0.005f);
+		}
 	}
 
 	public void OnFoeHealthChanged(int newHealth)
 	{
 		GD.Print($"Updating Health to {newHealth}");
+		if (lifebarProgress.Value - newHealth > 0)
+		{
+			DamageTakenText.Text = "-"+((int)(lifebarProgress.Value - newHealth)).ToString();
+			DamageTakenText.SelfModulate = new Color(DamageTakenText.SelfModulate, 1);
+		}
 		lifebarProgress.Value = newHealth;
 		if (lifebarProgress.Value != lifebarProgress.MaxValue)
 		{
