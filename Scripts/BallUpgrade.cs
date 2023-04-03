b8931059940e5bd8000a15c2bbdead8246754892
Arthur Cousseau <arthur.cousseau@me.com>
2023-04-04 00:27:55 +0200

    Move ball upgrade stuff to proper file

---
+++ Scripts/BallUpgrade.cs
@@ -0,0 +1,103 @@
+using Godot;
+using System;
+
+/// <summary>
+/// This is because Godot won't recognize the class for things like Signals,
+/// if the class doesn't inherit from GodotObject.
+/// So we wrap it to be able to still use the underlying class as freely as possible.
+/// </summary>
+public partial class BallUpgradeGD : GodotObject
+{
+    public BallUpgradeGD(BallUpgrade upgrade)
+    {
+        Upgrade = upgrade;
+    }
+
+    public BallUpgrade Upgrade { get; }
+}
+
+public class BallUpgrade
+{
+    private const string ImagesRoot = "res://Sprites/UpgradeItems";
+
+    public BallUpgrade(
+        string description,
+        Action<BallManager> effect,
+        Predicate<BallManager> condition,
+        string imageName)
+    {
+        Description = description;
+        Effect = effect;
+        Condition = condition;
+        ImageName = imageName;
+    }
+
+    public string Description { get ; }
+    public Action<BallManager> Effect { get; }
+    public Predicate<BallManager> Condition { get; }
+    public string ImageName { get; }
+
+    public CompressedTexture2D Image
+    {
+        get
+        {
+            _image ??= GD.Load<CompressedTexture2D>($"{ImagesRoot}/{ImageName}.png");
+            return _image;
+        }
+    }
+    private CompressedTexture2D _image = null;
+}
+
+public static class BallUpgrades
+{
+    public static BallUpgrade PickRandomUpgrade()
+    {
+        return Upgrades[GD.Randi() % Upgrades.Length];
+    }
+
+    public static readonly BallUpgrade[] Upgrades =
+    {
+        new(
+            "Increase radius",
+            (manager) => manager.IncreaseRadius(),
+            (manager) => !manager.IsRadiusMax(),
+            "radius_increase"
+        ),
+        new(
+            "Decrease radius",
+            (manager) => manager.DecreaseRadius(),
+            (manager) => !manager.IsRadiusMin(),
+            "radius_decrease"
+        ),
+        new(
+            "Increase rotation speed",
+            (manager) => manager.IncreaseRotationSpeed(),
+            (manager) => !manager.IsRotationSpeedMax(),
+            "rotation_increase"
+        ),
+        new(
+            "Decrease rotation speed",
+            (manager) => manager.DecreaseRotationSpeed(),
+            (manager) => !manager.IsRotationSpeedMin(),
+            "rotation_decrease"
+        ),
+        new(
+            "Add 2 more balls",
+            (manager) => manager.AddTwoMoreBalls(),
+            (manager) => !manager.IsNumberOfBallsMax(),
+            "ball_increase"
+        ),
+        new(
+            "Remove 2 balls",
+            (manager) => manager.RemoveTwoBalls(),
+            (manager) => !manager.IsNumberOfBallsMin(),
+            "ball_decrease"
+        ),
+        new(
+            "Increase balls power",
+            (manager) => manager.IncreaseBallsPower(),
+            (manager) => true,
+            "damage_increase"
+        )
+    };
+}
--- Scripts/PowerUpManager.cs
+++ Scripts/PowerUpManager.cs
@@ -1,71 +1,14 @@
 using Godot;
-using System;
-using System.Collections.Generic;
 
 public partial class PowerUpManager : Node2D
 {
 	[Export] private BallManager ballManager;
 
-	private class BallUpgrade
-	{
-		public string Description;
-		public Action<BallManager> Effect;
-		public Func<BallManager, bool> Condition;
-	}
-
-	private BallUpgrade[] upgrades = {
-		new BallUpgrade {
-			Description = "Increase radius",
-			Effect = (BallManager manager) => manager.IncreaseRadius(),
-			Condition = (BallManager manager) => !manager.IsRadiusMax()
-		},
-		new BallUpgrade {
-			Description = "Decrease radius",
-			Effect = (BallManager manager) => manager.DecreaseRadius(),
-			Condition = (BallManager manager) => !manager.IsRadiusMin()
-		},
-		new BallUpgrade {
-			Description = "Increase rotation speed",
-			Effect = (BallManager manager) => manager.IncreaseRotationSpeed(),
-			Condition = (BallManager manager) => !manager.IsRotationSpeedMax()
-		},
-		new BallUpgrade {
-			Description = "Decrease rotation speed",
-			Effect = (BallManager manager) => manager.DecreaseRotationSpeed(),
-			Condition = (BallManager manager) => !manager.IsRotationSpeedMin()
-		},
-		new BallUpgrade {
-			Description = "Add 2 more balls",
-			Effect = (BallManager manager) => manager.AddTwoMoreBalls(),
-			Condition = (BallManager manager) => !manager.IsNumberOfBallsMax()
-		},
-		new BallUpgrade {
-			Description = "Remove 2 balls",
-			Effect = (BallManager manager) => manager.RemoveTwoBalls(),
-			Condition = (BallManager manager) => !manager.IsNumberOfBallsMin()
-		},
-		new BallUpgrade {
-			Description = "Increase balls power",
-			Effect = (BallManager manager) => manager.IncreaseBallsPower(),
-			Condition = (BallManager manager) => true
-		}
-	};
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
 	public void OnPlayerLevelChanged(int newLevel)
 	{
 		while (true)
 		{
-			var upgrade = upgrades[GD.Randi() % upgrades.Length];
+			var upgrade = BallUpgrades.PickRandomUpgrade();
 			if (!upgrade.Condition(ballManager))
 				continue;
 
